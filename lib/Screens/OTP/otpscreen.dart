c537d92c3224e3c1a2e3a9a1d718bb9aa2603d07
Dark angel <50552311+darkamgel@users.noreply.github.com>
2022-02-02 22:11:29 +0545

    OTP Verification using country code

---
+++ lib/Screens/OTP/otpscreen.dart
@@ -0,0 +1,96 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:khan_pin/Screens/homescreen.dart';
+import 'package:pinput/pin_put/pin_put.dart';
+
+class OTPControllerScreen extends StatefulWidget {
+  final String phone;
+  final String codeDigits;
+
+  OTPControllerScreen({required this.phone, required this.codeDigits});
+
+  @override
+  State<OTPControllerScreen> createState() => _OTPControllerScreenState();
+}
+
+class _OTPControllerScreenState extends State<OTPControllerScreen> {
+  final GlobalKey<ScaffoldState> _scaffoldkey = GlobalKey<ScaffoldState>();
+  final TextEditingController _pinOTPCodeController = TextEditingController();
+  final FocusNode _pinOTPCodeFocus = FocusNode();
+  String? verificationCode;
+
+  final BoxDecoration pinOTPCodeDecoration = BoxDecoration(
+      color: Colors.blueAccent,
+      borderRadius: BorderRadius.circular(10.0),
+      border: Border.all(
+        color: Colors.grey,
+      ));
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldkey,
+      appBar: AppBar(
+        title: Text("OTP Verification"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Image.asset("images/otp.jpg"),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 20),
+            child: Center(
+              child: GestureDetector(
+                onTap: () {},
+                child: Text(
+                  "Verifying : ${widget.codeDigits} - ${widget.phone}",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(40.0),
+            child: PinPut(
+              fieldsCount: 6,
+              textStyle: TextStyle(fontSize: 25.0, color: Colors.white),
+              eachFieldHeight: 55.0,
+              eachFieldWidth: 40.0,
+              focusNode: _pinOTPCodeFocus,
+              controller: _pinOTPCodeController,
+              submittedFieldDecoration: pinOTPCodeDecoration,
+              selectedFieldDecoration: pinOTPCodeDecoration,
+              followingFieldDecoration: pinOTPCodeDecoration,
+              pinAnimationType: PinAnimationType.rotation,
+              onSubmit: (pin) async {
+                try {
+                  await FirebaseAuth.instance
+                      .signInWithCredential(PhoneAuthProvider.credential(
+                          verificationId: verificationCode!, smsCode: pin))
+                      .then((value) {
+                    if (value.user != null) {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(builder: (c) => HomeScreen()),
+                      );
+                    }
+                  });
+                } catch (e) {
+                  FocusScope.of(context).unfocus();
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text("Invalid OTP"),
+                    duration: Duration(seconds: 3),
+
+                    ),
+                  );
+                }
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/Screens/loginScreen.dart
@@ -0,0 +1,94 @@
+import 'package:country_code_picker/country_code_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:khan_pin/Screens/OTP/otpscreen.dart';
+
+class LoginScreen extends StatefulWidget {
+  LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  String dialCodeDigits = "+00";
+  TextEditingController _controller = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 100,
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 28.0, right: 20),
+              child: Image.asset("images/logo.png"),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10),
+              child: Center(
+                child: Text(
+                  "Phone (OTP) Authentication",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            SizedBox(
+              width: 400,
+              height: 400,
+              child: CountryCodePicker(
+                onChanged: (country) {
+                  setState(() {
+                    dialCodeDigits = country.dialCode!;
+                  });
+                },
+                initialSelection: "NP",
+                showCountryOnly: false,
+                showOnlyCountryWhenClosed: false,
+                favorite: ["+1", "US", "+92", "PAK", "+977", "NP"],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10.0, left:10.0 , right: 10.0,),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Phone Number",
+                  prefix: Padding(padding: EdgeInsets.all(4),
+                  child: Text(dialCodeDigits),
+
+                  ),
+                ),
+                maxLength: 10,
+                keyboardType: TextInputType.number,
+                controller: _controller,
+              ),
+            ),
+
+            Container(
+              margin: EdgeInsets.all(15),
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: (){
+
+                  Navigator.of(context).push(MaterialPageRoute(builder: (c) =>OTPControllerScreen(
+                    phone: _controller.text,
+                    codeDigits: dialCodeDigits,
+                  )));
+                },
+
+
+
+                child: Text("Next" , style:TextStyle(color: Colors.white , fontWeight: FontWeight.bold),),),
+            )
+
+          ],
+        ),
+      ),
+    );
+  }
+}
