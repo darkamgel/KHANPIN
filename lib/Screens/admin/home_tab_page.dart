85150daf056d8017b6d51f77066aaf1ab86d68fe
Dark angel <50552311+darkamgel@users.noreply.github.com>
2022-04-15 09:19:38 +0545

    .

---
+++ lib/Screens/admin/home_tab_page.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:khan_pin/Screens/admin/homescreenadmin.dart';
+import 'package:khan_pin/Screens/users/OTP/favorite_page.dart';
+import 'package:khan_pin/Screens/users/OTP/order_page.dart';
+import 'package:khan_pin/Screens/users/OTP/profile_page.dart';
+import '../../../widgets/Map_Box.dart';
+
+
+class MainHomePageAdmin extends StatefulWidget {
+
+  static const String idscreen = "admin_tab";
+  @override
+  _MainHomePageState createState() => _MainHomePageState();
+}
+
+class _MainHomePageState extends State<MainHomePageAdmin> {
+  int currentIndex = 0;
+
+  List<Widget> pages = [];
+  late Widget currentPage;
+  late Homepageadmin homePage;
+  late ProfilePage profilePage;
+  late Order orderPage;
+  late FavoritePage favPage;
+  late GoogleMapsDraw maps;
+
+  @override
+  void initState() {
+    homePage = Homepageadmin();
+    
+    profilePage = ProfilePage();
+    favPage = FavoritePage();
+    maps = GoogleMapsDraw();
+
+    pages = [homePage,  favPage, maps, profilePage];
+
+    currentPage = homePage;
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: (int index) {
+            setState(() {
+              currentIndex = index;
+              currentPage = pages[index];
+            });
+          },
+          currentIndex: currentIndex,
+          type: BottomNavigationBarType.fixed,
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            
+            BottomNavigationBarItem(icon: Icon(Icons.note_alt), label: "Orders"),
+            BottomNavigationBarItem(icon: Icon(Icons.map), label: "Maps"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person_outline), label: "Logout"),
+          ],
+        ),
+        body: currentPage,
+      ),
+    );
+  }
+}
